6fbef8c9d4ec49c0da93404e8e8cf3fb85563842
Kirill Berezin <enelar@develop-project.ru>
2014-04-06 06:13:39 +0400

    Database connection

--- rpc.php
+++ rpc.php
@@ -2,6 +2,9 @@
 
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
+include_once('pgsql_php/connect.php');
+new db("dbname=exchange host=localhost user=postgres");
+
 function phoxy_conf()
 {
   $ret = phoxy_default_conf();
